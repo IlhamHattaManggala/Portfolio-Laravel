e51b59267b38796e0782cecb97844a5d35e25975
IlhamHattaManggala <ilhamhattamanggala31@gmail.com>
2026-09-02 13:58:06 +0700

    perf(lighthouse): add display=swap to fonts and jfif image caching in .htaccess
    - Add &display=swap to Bunny Fonts link in app.blade.php to resolve LCP render-blocking element delay
    - Add .jfif image extension to mod_expires and mod_headers Cache-Control rules in public/.htaccess

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -34,7 +34,7 @@
         <link rel="icon" href="/images/profile.webp" type="image/webp">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600&display=swap" rel="stylesheet" />
 
         @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
